3d369727d03f23617a74f1e8d5ab8615c56749a2
Aquele_Cara <122459409+Igor-CA@users.noreply.github.com>
2023-06-19 11:59:50 -0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,16 +1,10 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
-/*
-const firebaseConfig = {
-  projectId: process.env.REACT_APP_PROJECT_ID,
-};
-*/
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: "find-these-characters", //TODO: Not working with .env for some reason
+  projectId: "find-these-characters", 
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID
